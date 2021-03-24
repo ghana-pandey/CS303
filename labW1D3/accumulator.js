a975feff71ada3320ba48bb001571946ac3bfb44
Ghana Pandey <pandeyghanashyam0@gmail.com>
2021-03-24 16:51:13 -0500

    assignment

--- labW1D3/accumulator.js
+++ labW1D3/accumulator.js
@@ -5,7 +5,23 @@
 
  * @returns {Accumulator} constructor function
  */
-function Accumulator(){
-//implement this
+ function Accumulator(initialValue,increment){
+    this.currentValue=initialValue
+    this.increment=increment
+
+    
+this.accumulate=function(){
+   this.currentValue= this.currentValue+this.increment
+   return this.currentValue;
+   
 
 }
+this.report=function(){
+    return this.currentValue
+}
+}
+let c=new Accumulator(5,10);
+console.log(c.accumulate());
+console.log(c.accumulate());
+
+console.log(c.report());
--- labW1D3/calc.js
+++ labW1D3/calc.js
@@ -4,5 +4,21 @@
  * @returns {Calculator} this is a constructor function
  */
 function Calculator() {
- //implement this
-  }
+ function getValue(c,d){
+     this.a=c;
+     this.b=d;
+
+ }
+this.sum=function(){
+    return this.a+this.b;
+}
+this.mul=function(){
+    
+     return this.a*this.b
+ }
+}
+ let calc=new Calculator();
+ calc.getValue(4,5);
+calc.sum();
+calc.mul();
+
