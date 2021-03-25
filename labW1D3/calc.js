7b4bc4229477b2e39d7a0ba2403fc3ee0ea630f1
Ghana Pandey <pandeyghanashyam0@gmail.com>
2021-03-25 13:53:27 -0500

    Update calc.js

--- labW1D3/calc.js
+++ labW1D3/calc.js
@@ -1,24 +1,19 @@
-"use strict"
-
-/**
- * @returns {Calculator} this is a constructor function
- */
 function Calculator() {
- function getValue(c,d){
-     this.a=c;
-     this.b=d;
-
- }
-this.sum=function(){
-    return this.a+this.b;
-}
-this.mul=function(){
-    
-     return this.a*this.b
- }
-}
- let calc=new Calculator();
- calc.getValue(4,5);
-calc.sum();
-calc.mul();
-
+    this.getValue=function(c,d)
+    {
+        this.a=c;
+        this.b=d;
+   
+    }
+   this.sum=function(){
+       return this.a+this.b;
+   }
+   this.mul=function(){
+       
+        return this.a*this.b
+    }
+   }
+    let calc=new Calculator();
+    calc.getValue(4,5);
+   console.log(calc.sum());
+   console.log(calc.mul());
